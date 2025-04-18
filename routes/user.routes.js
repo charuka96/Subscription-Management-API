369e00ea5c75b0ef82d1e57287d6658a05a495f4
charuka96 <chaukathisaru96@gmail.com>
2025-04-18 14:19:49 +0530

    get users and user

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,10 +1,12 @@
 import { Router } from "express";
+import { getUser, getUsers } from "../controllers/user.controller.js";
+
 
 const userRouter = Router();
 
-userRouter.get('/',(req,res)=>res.send({title:'GET all user'}) );
+userRouter.get('/',getUsers);
 
-userRouter.get('/:id' ,(req,res)=>res.send({title:'GET  user dealails'}) );
+userRouter.get('/:id',getUser);
 
 userRouter.post('/' ,(req,res)=>res.send({title:'create new user'}) );
 
